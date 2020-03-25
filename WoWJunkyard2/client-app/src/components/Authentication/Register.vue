40e47cec4bd5a833862d760b7873749f37249ae0
petar <peterst@abv.bg>
2020-03-26 00:46:54 +0200

    - Added API call in Register component with Axios and Vuex. - Added several validations in Register component using Vuelidate library.

--- WoWJunkyard2/client-app/src/components/Authentication/Register.vue
+++ WoWJunkyard2/client-app/src/components/Authentication/Register.vue
@@ -3,7 +3,7 @@
       <template v-slot:activator="{ on }">
         <v-btn color="succes" dark v-on="on">Register</v-btn>
       </template>
-      <v-form>
+      <v-form @submit.prevent="register">
         <v-card
         class="mx-auto"
         max-width="300"
@@ -44,7 +44,7 @@
             @blur="$v.password.$touch()"
           ></v-text-field>
 
-        <v-btn class="mr-4" color="succes" >Create</v-btn>
+        <v-btn class="mr-4" color="succes" type="submit" @click="submit" :disabled="$v.$invalid">Create</v-btn>
         <v-btn color="succes" @click="dialog = false; clear();">Close</v-btn>
         </v-card>
       </v-form>
@@ -62,7 +62,17 @@
       email: { required, email },
       username: { required, maxLength: maxLength(10) },
       displayName: { required, maxLength: maxLength(10) },
-      password: { required, minLength: minLength(6)},
+      password: { 
+        required,
+        minLength: minLength(6),
+        strongPassword(password) {
+          return (
+            /[a-z]/.test(password) && // checks for upper character
+            /[0-9]/.test(password) && // checks for 0-9
+            /\W|_/.test(password) // checks for special character
+          );
+        }
+      },
     },
     data: () => ({
       dialog: false,
@@ -73,59 +83,62 @@
       password: "",
     }),
     computed: {
-      emailErrors () {
+      emailErrors() {
         const errors = []
         if (!this.$v.email.$dirty) return errors
         !this.$v.email.email && errors.push('Must be valid e-mail')
         !this.$v.email.required && errors.push('E-mail is required')
         return errors
       },
-      usernameErrors () {
+      usernameErrors() {
         const errors = []
         if (!this.$v.username.$dirty) return errors
         !this.$v.username.maxLength && errors.push('Name must be at most 10 characters long')
         !this.$v.username.required && errors.push('Name is required.')
         return errors
       },
-      displayNameErrors () {
+      displayNameErrors() {
         const errors = []
         if (!this.$v.displayName.$dirty) return errors
         !this.$v.displayName.maxLength && errors.push('Display name must be at most 10 characters long')
         !this.$v.displayName.required && errors.push('Display name is required.')
         return errors
       },
-      passwordErrors () {
+      passwordErrors() {
         const errors = []
         if (!this.$v.password.$dirty) return errors
-        !this.$v.password.minLength && errors.push('Password must be at least 6 characters long')
-        !this.$v.password.required && errors.push('Password is required.')
+        !this.$v.password.minLength && errors.push('Password must be at least 6 characters long');
+        !this.$v.password.required && errors.push('Password is required.');
+        !this.$v.password.strongPassword && errors.push('Strong passwords must have an upper letter, a number and a special character');
         return errors
       },
+      // submitErrors() {
+      //   const errors = []
+      //   if (!this.isSubmitValid) return errors
+      //   !this.isSubmitValid && errors.push('Enter valid data!');
+      //   return errors
+      // },
     },
     methods:{
       submit () {
         this.$v.$touch()
       },
-      clear () {
+      clear() {
         this.$v.$reset()
         this.email = ''
         this.username = ''
         this.displayName = ''
         this.password = ''
       },
+      register(){
+        this.$store.dispatch('register',{
+          email: this.email,
+          username: this.username,
+          displayName: this.displayName,
+          password: this.password,
+        })
+      }
     },
-    // created(){
-    //   axios.get('http://localhost:5000/api/user')
-    //     .then(function (response) {
-    //       console.log(response);
-    //     })
-    //     .catch(function (error) {
-    //       console.log(error);
-    //     })
-    //     .then(function () {
-    //     })
-    // }
-
   }
 </script>
 
--- WoWJunkyard2/client-app/src/store/index.js
+++ WoWJunkyard2/client-app/src/store/index.js
@@ -1,14 +1,26 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+
   },
   mutations: {
   },
   actions: {
+    async register({ commit },credentials){
+      try {
+        const response = await axios.post("http://localhost:5000/api/user/register", credentials)
+        console.dir(response)
+        commit('SET_USER_DATA', response.data)
+      }
+      catch (error) {
+        console.dir(error.response)
+      }             
+    } 
   },
   modules: {
   }
