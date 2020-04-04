52be800085f9aacaf29a5fb6ef7ca9feaf550a51
petar <peterst@abv.bg>
2020-04-04 22:41:04 +0300

    - Updated authentication component - Added Welcome text and now displaying username of logged in user. - Added logout functionallity.

--- WoWJunkyard2/client-app/src/components/Authentication/Authentication.vue
+++ WoWJunkyard2/client-app/src/components/Authentication/Authentication.vue
@@ -1,46 +1,60 @@
 <template>
-  <v-card
-    class="mx-auto"
-    max-width="300"
-    :dark="true"
-    tile>
-    <AppLogin v-if="!loggedIn"/> 
-    <AppRegister v-if="!loggedIn"/>   
-    <p v-if="loggedIn">Welcome back!</p>   
+  <v-card class="mx-auto" :dark="true" tile>
+    <AppLogin v-if="!loggedIn" />
+    <AppRegister v-if="!loggedIn" />
+    <div v-if="loggedIn" class="row welcomeContainer">
+      <div class="welcomeMsg align-middle">Welcome back, {{loggedInUser}}!</div>
+      <v-btn class="align-middle" color="succes" @click="logout">Logout</v-btn>
+    </div>
   </v-card>
 </template>
 
 <script>
-import AppRegister from './Register.vue'
-import AppLogin from './Login.vue'
-import {authenticatedComputed} from '../../store/helpers.js'
+import AppRegister from "./Register.vue";
+import AppLogin from "./Login.vue";
+import { authenticatedComputed } from "../../store/helpers.js";
+import {usernameComputed} from '../../store/helpers.js';
 
 export default {
-  name: 'authentication',
+  name: "authentication",
   components: {
     AppRegister,
     AppLogin
   },
-  computed:{
-    ...authenticatedComputed
+  computed: {
+    ...authenticatedComputed,
+    ...usernameComputed
+  },
+  methods: {
+    logout() {
+      this.$store.dispatch("logout");
+    },
   },
   data: () => ({
+    
+  }),
 
-  }) 
-}
+};
 </script>
 
 <style scoped>
-.v-list-item__title{
+.welcomeContainer{
+  display:contents;
+}
+.welcomeMsg{
+  display:inline-block;
+  padding:10px;
+}
+.v-list-item__title {
   line-height: 1.3 !important;
 }
-.v-list-item__icon{
+.v-list-item__icon {
   margin: 10px 0 !important;
 }
-.v-item-group{
-  display:flex;
+.v-item-group {
+  display: flex;
 }
-.v-card{
-  float:right;
+.v-card {
+  float: right;
 }
 </style>
